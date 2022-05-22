1b4316cd3cb104d91e5e79c6ddd70b2e0538b070
Olivia Lind <eo.lind@gmail.com>
2022-05-21 20:12:01 -0500

    each die is returning random number

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 // Put your code here
+
+console.log("Let's roll some dice, baby!");
+console.log("---------------------------");
+console.log("");
+
+
+const roll = () => {
+    return Math.floor(Math.random() * 6) + 1
+}
+
+const die1 = roll();
+const die2 = roll();
+
+console.log(die1);
+console.log(die2);
+
+
